c96a28818e22660d8f10b3f951816a55ef17e6e1
kst1040@gmail.com <kst1040@gmail.com>
2022-10-05 14:49:20 +0900

    프로젝트시작

---
+++ app/src/main/java/kr/ac/kumoh/s20170187com/example/w0501carddealer/MainActivity.kt
@@ -0,0 +1,15 @@
+package kr.ac.kumoh.s20170187com.example.w0501carddealer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.s20170187com.example.w0501carddealer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+}
