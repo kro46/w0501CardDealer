05487da844eee03d0f0a9a19d647e06ae8c3aa15
kst1040@gmail.com <kst1040@gmail.com>
2022-10-05 15:51:33 +0900

    카드번호 로그

--- app/src/main/java/kr/ac/kumoh/s20170187com/example/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170187com/example/w0501carddealer/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20170187com.example.w0501carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20170187com.example.w0501carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.card1.setImageResource(R.drawable.c_jack_of_spades)
+        getCardName(32)
+    }
+
+    private fun getCardName(c: Int):String {
+        Log.i("getCardName",c.toString())
+        val shape = when(c/13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+        Log.i("getCardName",shape)
+        return "c_4_of_spades"
     }
 }
