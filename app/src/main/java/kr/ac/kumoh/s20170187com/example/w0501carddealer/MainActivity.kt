7f2365c60d9e1df41b2871c3993d8868b948392d
kst1040@gmail.com <kst1040@gmail.com>
2022-10-05 15:38:25 +0900

    레이이웃 분리

--- app/src/main/java/kr/ac/kumoh/s20170187com/example/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170187com/example/w0501carddealer/MainActivity.kt
@@ -8,8 +8,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
+
+        binding.card1.setImageResource(R.drawable.c_jack_of_spades)
     }
 }
