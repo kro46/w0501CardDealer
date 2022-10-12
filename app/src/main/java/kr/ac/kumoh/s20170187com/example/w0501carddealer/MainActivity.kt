d1280cede81f40dfb2fffeab73ce0c25a0ff7a24
kst1040@gmail.com <kst1040@gmail.com>
2022-10-12 15:16:18 +0900

    카드게임완성

--- app/src/main/java/kr/ac/kumoh/s20170187com/example/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170187com/example/w0501carddealer/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kr.ac.kumoh.s20170187com.example.w0501carddealer.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var  binding: ActivityMainBinding
@@ -14,20 +15,42 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        binding.card1.setImageResource(R.drawable.c_jack_of_spades)
-        getCardName(32)
+        binding.btnDeal.setOnClickListener{
+            val res = IntArray(5)
+            for(i in res.indices){
+            val c = IntArray(5)
+             res[i] = resources.getIdentifier(
+                getCardName(Random.nextInt(52)),
+                "drawable",packageName)
+            }
+            binding.card1.setImageResource(res[0])
+            binding.card2.setImageResource(res[1])
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])
+        }
+        //binding.card1.setImageResource(R.drawable.c_jack_of_spades
     }
 
     private fun getCardName(c: Int):String {
         Log.i("getCardName",c.toString())
-        val shape = when(c/13) {
+        val shape = when(c / 13) {
             0 -> "spades"
             1 -> "diamonds"
             2 -> "hearts"
             3 -> "clubs"
             else -> "error"
         }
-        Log.i("getCardName",shape)
-        return "c_4_of_spades"
+
+        val number = when(c % 13){
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        //Log.i("getCardName",shape)
+        return "c_${number}_of_${shape}"
     }
 }
